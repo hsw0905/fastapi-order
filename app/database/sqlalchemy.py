96255f9672d1d78cff7a97acfdbdb56fa89372c2
hsw0905 <hsw0905@gmail.com>
2023-11-24 15:25:37 +0900

    pytest, sqlalchemy

---
+++ app/database/sqlalchemy.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+from app import config
+
+engine = create_engine(url=config.DB_URL, echo=config.DB_ECHO, pool_pre_ping=config.DB_PRE_PING)
+
+session_factory = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+session = scoped_session(session_factory=session_factory)
---
+++ tests/conftest.py
@@ -0,0 +1,70 @@
+import os
+from typing import AsyncGenerator, Generator
+
+import pytest
+import pytest_asyncio
+from asgi_lifespan import LifespanManager
+from fastapi import FastAPI
+from httpx import AsyncClient
+from uvloop import Loop, new_event_loop
+
+from app import create_app
+from app.database.sqlalchemy import engine, session
+from core.persistence.models.base import Base
+
+
+@pytest.fixture(scope="session")
+def app() -> FastAPI:
+    return create_app()
+
+
+@pytest_asyncio.fixture(scope="session")
+async def running_app(app: FastAPI) -> AsyncGenerator[FastAPI, None]:
+    async with LifespanManager(app=app):
+        yield app
+
+
+@pytest.fixture(scope="session")
+def event_loop() -> Generator[Loop, None, None]:
+    """pytest->event_loop fixture를 override 하기 위한 코드"""
+    loop = new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture(scope="session")
+async def async_client(running_app: FastAPI) -> AsyncGenerator[AsyncClient, None]:
+    async with AsyncClient(app=running_app, base_url="http://test") as client:
+        yield client
+
+
+def is_sqlite_used(database_url: str) -> bool:
+    if ":memory:" in database_url:
+        return True
+    return False
+
+
+def _is_local_db_used(database_url: str) -> None:
+    """
+    local db를 사용하면 memory db 삭제
+    """
+    if ":memory:" not in database_url:
+        if os.path.exists(database_url.split("sqlite:///")[-1]):  # :memory:
+            os.unlink(database_url.split("sqlite:///")[-1])
+
+
+def test_session():
+    _is_local_db_used(str(engine.url))
+
+    connection = engine.connect()
+    transaction = connection.begin()
+
+    if is_sqlite_used(str(engine.url)):
+        Base.metadata.drop_all(engine)
+        Base.metadata.create_all(engine)
+
+    yield session
+
+    transaction.rollback()
+    connection.close()
+
